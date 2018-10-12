ed91c6744daa72a0bb2a01738d2e0d173319e22a
nn4343 <gea01441@nifty.com>
2018-10-13 08:42:05 +0900

    no message

---
+++ _test/m0/TestControlUnit.pas
@@ -0,0 +1,73 @@
+﻿unit TestControlUnit;
+
+interface
+
+uses StateManagerUnit;
+
+type TestControl = class(StateManager)
+
+  protected
+
+    fbYesNo : boolean;
+
+    procedure br_YES(const st: string);
+    procedure br_NO(const st: string);
+
+    procedure S_START(const bFirst : boolean);
+    procedure S_END(const bFirst : boolean);
+
+    procedure _execFunc(const st: string; bFirst : boolean); override;
+
+  public
+    procedure Start; override;
+    function  IsEnd : boolean; override;
+
+end;
+
+implementation
+
+procedure TestControl.br_YES(const st: string);
+begin
+    if HasNextState() = false then begin
+        if fbYesNo = true then SetNextState(st);
+    end;
+end;
+
+procedure TestControl.br_NO(const st: string);
+begin
+    if HasNextState() = false then begin
+        if fbYesNo = false then SetNextState(st);
+    end;
+end;
+
+
+procedure TestControl.S_START(const bFirst : boolean);
+begin
+  if bFirst=true then WriteLn('S_START');
+  GotoState('S_END');
+end;
+procedure TestControl.S_END(const bFirst : boolean);
+begin
+  if bFirst=true then WriteLn('S_END');
+end;
+
+procedure TestControl._execFunc(const st: string; bFirst: Boolean);
+label end_proc;
+begin
+  if st = 'S_START' then begin S_START(bFirst); goto end_proc;  end;
+  if st = 'S_END'   then begin S_END(bFirst);   goto end_proc;  end;
+  end_proc:
+end;
+
+procedure TestControl.Start;
+begin
+  GotoState('S_START');
+end;
+
+function TestControl.IsEnd;
+begin
+  Result := CheckState('S_END');
+end;
+
+
+end.
---
+++ _test/m0/TestControl_createdUnit.pas
@@ -0,0 +1,47 @@
+﻿unit TestControlUnit;
+
+interface
+
+type TestControl = class(TestControl)
+    private
+      procedure S_START(const bFirst : boolean);
+      procedure S_END(const bFirst : boolean);
+      procedure _execFunc(const st: string; bFirst : boolean); override;
+
+    public
+      procedure Start; override;
+      function  IsEnd : boolean; override;
+end;
+
+implementation
+
+procedure TestControl_created.S_START(const bFirst : boolean);
+begin
+  if bFirst=true then WriteLn('S_START');
+  GotoState('S_END');
+end;
+procedure TestControl_created.S_END(const bFirst : boolean);
+begin
+  if bFirst=true then WriteLn('S_END');
+end;
+
+procedure TestControl_created._execFunc(const st: string; bFirst: Boolean);
+label end_proc;
+begin
+  if st = 'S_START' then begin S_START(bFirst); goto end_proc;  end;
+  if st = 'S_END'   then begin S_END(bFirst);   goto end_proc;  end;
+  end_proc:
+end;
+
+procedure TestControl_created.Start;
+begin
+  GotoState('S_START');
+end;
+
+function TestControl_created.IsEnd;
+begin
+  Result := CheckState('S_END');
+end;
+
+
+end.
